45456b3de7a7625a4d5052f8e163fcb99c607f09
Josh Palmer <josh-palmer@live.com>
2021-01-06 20:43:37 +0800

    added background

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ request.onload = () => {
     console.log("json request success");
     json = request.response;
 }
+
+let flashcardState = "unselected";
+const flashcardOptions = document.getElementById("flashcard-options");
+const flashcardCont = document.getElementById("flashcard-container");
+flashcardOptions.addEventListener("click", e => {
+    if (e.target.className !== "option-button") {
+        return;
+    } else {
+        switch (e.target.id) {
+            case "hiragana-flash-button":
+                flashcardState = "hiragana";
+                break;
+            case "katakana-flash-button":
+                flashcardState = "katakana";
+                break;
+            case "all-flash-button":
+                flashcardState = "all";
+                break;
+            default:
+                break;
+        }
+        flashcardOptions.style.display = "none";
+    }
+})
